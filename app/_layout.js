59bd7c29bb05e6d3ac2e7da9abc12866c75769ae
Samuel Emeka <samuel.emeka@kibo.school>
2024-04-03 01:49:36 +0100

    simesta dev env setup

---
+++ app/_layout.js
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+
+
+    return <Stack />
+}
+
+export default Layout;
