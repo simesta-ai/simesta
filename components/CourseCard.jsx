acfe971082afcb9f4bc60849f2ccef5eae19250b
Samuel Emeka <samuel.emeka@kibo.school>
2024-06-20 22:05:08 +0100

    clean up

--- components/CourseCard.jsx
+++ components/CourseCard.jsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, TouchableOpacity, Image, Pressable } from 'react-native'
+import { useRouter } from 'expo-router'
 import RoundProgressBar from './dashboard/RoundProgressBar'
 import HorizontalProgressBar from './dashboard/HorizontalProgressBar'
 
@@ -9,8 +10,14 @@ import { COLORS, SIZES } from '../constants'
 
 
 const CourseCard = ({ position, course }) => {
+
+  const router = useRouter()
+
+  const goToCourse = () => {
+    router.navigate(`/course/1`)
+  }
   return (
-    <Pressable style={styles.courseCardContainer(position)}>
+    <Pressable style={styles.courseCardContainer(position)} onPress={goToCourse}>
       {/* {position === "dashboard" && <RoundProgressBar value={course.progress} />} */}
       <View style={styles.imageContainer}><Image style={styles.image} source={course.image} resizeMode='cover' /></View>
       {/* <Text style={styles.categoryText(course.color)}>{course.category.toUpperCase()}</Text> */}
